a8673ee3525f00ab0e48151ed58e622b7322a81b
SaraaMiss <sara.kaouar.christol@gmail.com>
2023-09-15 14:59:29 +0200

    web client , room done

--- src/main/java/hotelservice/web/ReservationController.java
+++ src/main/java/hotelservice/web/ReservationController.java
@@ -3,13 +3,17 @@ package hotelservice.web;
 import hotelservice.models.Reservation;
 import hotelservice.repository.ReservationRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/reservations")
+@CrossOrigin("*")
 public class ReservationController {
     private ReservationRepository resarvationRepository;
 
